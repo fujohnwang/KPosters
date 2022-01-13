1ef562c44eddb533a9e402d151f575e9bf702c7c
afoo <afoo@afoo.me>
2022-01-13 19:21:54 +0800

    add svelte component directive demo

---
+++ src/posters_selector/templates/PayedTemplate.svelte
@@ -0,0 +1,7 @@
+<script>
+    import {content, signature} from "../store";
+</script>
+
+<div class="shadow-lg mx-auto relative max-w-md group cursor-pointer">
+    Payed Template : {$content}, {$signature}
+</div>
---
+++ src/posters_selector/templates/Template3.svelte
@@ -0,0 +1,9 @@
+<script>
+    import {content, signature} from "../store";
+</script>
+
+
+
+<div class="shadow-lg mx-auto relative max-w-md group cursor-pointer">
+    Template 3 : {$content}, {$signature}
+</div>
